9589729c8bebc33a465524c99771503a92deb4c8
Ernst Widerberg <ernstwi@kth.se>
2021-06-23 12:24:39 +0200

    Add a first API test

---
+++ api_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/steinfletcher/apitest"
+)
+
+var db *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	db, err = sql.Open("sqlite3", ":memory:") // In-memory database
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+		CREATE TABLE realm_signing_log (
+			serial INTEGER NOT NULL PRIMARY KEY,
+			realm TEXT NOT NULL,
+			ca_sub TEXT NOT NULL,
+			requester TEXT NOT NULL,
+			sub TEXT NOT NULL,
+			issued TEXT NOT NULL,
+			expires TEXT NOT NULL,
+			csr TEXT NOT NULL,
+			x509 TEXT,
+			revoked TEXT,
+			usage TEXT NOT NULL
+		);
+		INSERT INTO realm_signing_log VALUES (
+			1,
+			"realm_1",
+			"ca_sub_1",
+			"requester_1",
+			"sub_1",
+			"2020-06-23",
+			"2021-06-23",
+			"csr_1",
+			"x509_1",
+			NULL,
+			"usage_1"
+		);
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestGET(t *testing.T) {
+	apitest.New().
+		Handler(makeGETHandler(db)).
+		Get("/api/v0/noauth").
+		Expect(t).
+		Body(`[{
+			"serial": 1,
+			"realm": "realm_1",
+			"ca": "ca_sub_1",
+			"requester": "requester_1",
+			"subject": "sub_1",
+			"issued": "2020-06-23",
+			"expires": "2021-06-23",
+			"revoked": false,
+			"revoked_at": "",
+			"usage": "usage_1"
+		}]`).
+		Status(http.StatusOK).
+		End()
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ernstwi/letsrevoke
 
 go 1.16
 
-require github.com/mattn/go-sqlite3 v1.14.7
+require (
+	github.com/mattn/go-sqlite3 v1.14.7
+	github.com/steinfletcher/apitest v1.5.11
+)
